c4bd3352e4de7e4f6f818198a161f3437628b2b9
LunasSpace <ducksarenotus@gmail.com>
2026-08-21 21:47:27 +0100

    Added comments

--- loader.s
+++ loader.s
@@ -1,18 +1,18 @@
 global loader
 
-MAGIC_NUMBER equ 0x1BADB002
-FLAGS equ 0x0
-CHECKSUM equ -MAGIC_NUMBER
+MAGIC_NUMBER equ 0x1BADB002 ; define the magic number constant
+FLAGS equ 0x0 ; multiboot flags
+CHECKSUM equ -MAGIC_NUMBER ; calculate the checksum
+; (magic number + checksum + flags should equal 0)
 
 
-section .text:
-align 4
-    dd MAGIC_NUMBER
-    dd FLAGS
-    dd CHECKSUM
+section .text: ; start of the text (code) section
+align 4 ; the code must be 4 byte aligned
+    dd MAGIC_NUMBER ; write the magic number to the machine code,
+    dd FLAGS ; the flags,
+    dd CHECKSUM ; checksum
 
-loader:
-    mov eax, 0x0CAFEBABE
+loader: ; the loader label (defined as entry point in linker script)
+    mov eax, 0xCAFEBABE ; place the number 0xCAFEBABE in the register eax
 .loop:
-    jmp .loop
-    
+    jmp .loop ; loop forever
